5355ef9f9ef74381b6a3723c7f9f73cc04554199
BiassedXD <73149862+BiassedXD@users.noreply.github.com>
2021-02-02 19:41:18 -0800

    Add files via upload

---
+++ Site4/template-parts/header-styles.php
@@ -0,0 +1,65 @@
+<!-- header styles -->
+
+<link id="u-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+<style>.u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 979px;
+}
+.u-header .u-text-1 {
+  margin: 160px auto 0;
+}
+.u-header .u-image-1 {
+  width: 79px;
+  height: 79px;
+  margin: -66px 300px 0 auto;
+}
+.u-header .u-btn-1 {
+  border-style: none;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 0.875rem;
+  letter-spacing: 1px;
+  margin: 151px auto 60px;
+  padding: 28px 88px 29px 87px;
+}
+@media (max-width: 1199px) {
+  .u-header .u-image-1 {
+    width: 79px;
+    height: 79px;
+    margin-top: -66px;
+    margin-right: 192px;
+  }
+  .u-header .u-btn-1 {
+    margin-top: 237px;
+  }
+}
+@media (max-width: 991px) {
+  .u-header .u-image-1 {
+    margin-top: -66px;
+    margin-right: 90px;
+  }
+}
+@media (max-width: 767px) {
+  .u-header .u-image-1 {
+    margin-right: 0;
+  }
+  .u-header .u-btn-1 {
+    margin-top: 323px;
+  }
+}
+@media (max-width: 575px) {
+  .u-header .u-text-1 {
+    margin-top: 80px;
+    margin-left: 27px;
+    margin-right: 27px;
+  }
+  .u-header .u-image-1 {
+    margin-top: -59px;
+    margin-right: -50px;
+  }
+  .u-header .u-btn-1 {
+    margin-top: 222px;
+  }
+}</style>
